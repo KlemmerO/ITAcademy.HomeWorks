68ec6634b7f133a2906fa0d0866de01be13bb43d
Olga <Olga@NOTEBOOK_OLGA>
2020-06-05 19:54:13 +0200

    Addition/Subtraction/Multiplicatin/Division/Procent/Area

---
+++ HomeWorks/HW03.Task2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace HW03.Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+         {
+
+            Console.WriteLine("Hello World!");
+            Summ();
+        }
+        static double Summ()
+        {
+            return double.MiniValue;
+
+            Console.WriteLine("Hello World!");
+        }
+
+
+    }
+}
---
+++ HomeWorks/HW03.Task3/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HW03.Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataTime Get ()
+                return Datime
+        }
+    }
+}
