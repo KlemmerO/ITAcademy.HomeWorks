e795d9e1d9ef64dfdd97c3ec4d45f5fc0e2fb465
Olga <Olga@NOTEBOOK_OLGA>
2020-06-14 15:40:37 +0200

    Added

---
+++ HomeWorks04/hm_06/hm_06.Task01 (2)/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace hm_06.Task01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write the poem into one stroke with semicolon between");
+            string data = Console.ReadLine();
+            string[] separator = { ";"};
+            string[] strokes = data.Split (separator, StringSplitOptions.None);
+            foreach (var stroke in strokes)
+            Console.WriteLine (stroke.Replace("o", "a"));          
+            Console.ReadKey();
+        }
+    }
+}
